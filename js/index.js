bf1e12144f25d5b245d1a9632b84c57e9ee04f53
unknown <1184159464@qq.com>
2019-04-02 16:31:03 +0800

    index

---
+++ js/index.js
@@ -0,0 +1,11 @@
+//打开新的页面
+function openPage (obj,url,id ){
+			mui.plusReady(function(){
+			obj.addEventListener('tap',function(){
+				mui.openWindow({
+					url:url,
+					id:id
+				})
+			})
+		})
+		}
