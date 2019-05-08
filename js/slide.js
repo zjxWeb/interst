9abd755918c33aad75740cf04896e667090665ef
Hardisonxxx <ddeenngg@outlook.com>
2019-05-08 23:51:32 +0800

    m

--- js/slide.js
+++ js/slide.js
@@ -2,7 +2,7 @@
 mui.init()
 (function($) { 
 			$(".mui-scroll-wrapper").scroll({ 
-			bounce: true, //滚动条是否有弹力默认是true 
-			indicators: 0.006, //是否显示滚动条,默认是true 
+			bounce: true, 
+			indicators: 0.006, 
 			}); 
 			})(mui);
